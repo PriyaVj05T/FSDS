cc198b28e51f0bd1484b1e7775016c0d09659a4d
Priya_vijayvargiya <vijayvargiyapriya05@gmail.com>
2024-02-16 18:30:32 +0530

    logge file added

---
+++ src/DimondPricePrediction/logger.py
@@ -0,0 +1,16 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+log_path=os.path.join(os.getcwd(),"logs")
+
+os.makedirs(os.path.dirname(log_path),exist_ok=True)
+
+Log_file_path=os.path.join(log_path,LOG_FILE)
+
+logging.basicConfig(level=logging.INFO,
+                    filename=Log_file_path,
+                    format="[%(asctime)s] %(lineno)d %(name)s- %(levelname)s- %(message)s" 
+                              )
