9d4aa7300ca56fc8c7b5ca7972a228107b96424d
Priya_vijayvargiya <vijayvargiyapriya05@gmail.com>
2024-02-27 16:25:18 +0530

    model trainer code added and data transformation code modified

--- src/DimondPricePrediction/components/data_transformation.py
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -80,21 +80,33 @@ class DataTransformation:
             #segregate coloumns
             target_column_name ='price' 
             drop_columns=[target_column_name,'id']
+
             input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
             input_feature_test_df = test_df.drop(columns=drop_columns,axis=1)
+
             target_feature_train_df = train_df[target_column_name]
+            target_feature_test_df=  test_df[target_column_name]
 
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
             logging.info("Applying preprocessing object on train and test datasets")
 
+            train_arr= np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
+            test_arr= np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
+
+            
             save_object(
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj =preprocessing_obj
             )
 
+            logging.info("Preprocessing pickle file saved")
 
+            return (
+                train_arr,
+                test_arr
+            )
 
 
         except Exception as e:
